88e7d6152220367d1764479be0cda261f4eb2d08
Danielis Joniskis <dananasas@gmit.ie>
2019-03-23 17:44:41 +0000

    printing a out in binary

--- bits.c
+++ bits.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
 
+void binprint(uint32_t x){
+  int i;
+  uint32_t one = 1;
+
+  for(i = 0; i < 32; i++)
+    if((one << (31 - i)) & x)
+     printf("1");
+    else
+     printf("0");
+}
+
 int main(int argc, char *argv[]){
 
   uint32_t a = 1;
   uint32_t b = 2;
  
   printf(" a: %08x\n", a);
+  printf(" a: "); binprint(a); printf("\n");
   printf(" b: %08x\n", b);
 
   printf(" &: %08x\n", (a&b));
