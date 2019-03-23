ef478a5036cb071ee8791e7c0defb08383e2df92
Danielis Joniskis <dananasas@gmit.ie>
2019-03-23 23:01:08 +0000

    included 8 hexidecimal literals

--- sha256/sha256.c
+++ sha256/sha256.c
@@ -21,7 +21,24 @@ void sha256(){
   
   //Message schedule (Section 6.2).
   uint32_t W[64];
-  
   //Working variables (Section 6.2).
   uint32_t a, b, c, d, e, f, g, h;
+  //Two temporary variables (Section 6.2).
+  uint32_t T1, T2;
+
+  //The Hash value (Section 6.2)
+  //The values come from Section 5.3.3.
+  uint32_t H[8] = {
+    0x6a09e667
+    0xbb67ae85
+    0x3c6ef372
+    0xa54ff53a
+    0x510e527f
+    0x9b05688c
+    0x1f83d9ab
+    0x5be0cd19
+  };
+
+  
+
 }
