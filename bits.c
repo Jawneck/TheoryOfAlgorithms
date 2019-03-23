66ef36488291daca77d7f9df212426ad12e32376
Danielis Joniskis <dananasas@gmit.ie>
2019-03-23 17:57:14 +0000

    printing out bit strings in binary

--- bits.c
+++ bits.c
@@ -17,16 +17,23 @@ int main(int argc, char *argv[]){
   uint32_t a = 1;
   uint32_t b = 2;
  
-  printf(" a: %08x\n", a);
-  printf(" a: "); binprint(a); printf("\n");
-  printf(" b: %08x\n", b);
+//printf(" a: %08x\n", a);
+  printf("   a: "); binprint(a);    printf("\n");
+//printf(" b: %08x\n", b);
+  printf("   b: "); binprint(b);    printf("\n");
 
-  printf(" &: %08x\n", (a&b));
-  printf(" |: %08x\n", (a|b));
-  printf(" ^: %08x\n", (a^b));
-  printf(" ~: %08x\n", (~a));
-  printf("<<: %08x\n", (a<<1));
-  printf(">>: %08x\n", (a>>1));
+//printf(" &: %08x\n", (a&b));
+  printf(" a&b: "); binprint(a&b);  printf("\n");
+//printf(" |: %08x\n", (a|b));
+  printf(" a|b: "); binprint(a|b);  printf("\n");
+//printf(" ^: %08x\n", (a^b));
+  printf(" a^b: "); binprint(a^b);  printf("\n");
+//printf(" ~: %08x\n", (~a));
+  printf("  ~a: "); binprint(~a);   printf("\n");
+//printf("<<: %08x\n", (a<<1));
+  printf("a<<1: "); binprint(a<<1); printf("\n");
+//printf(">>: %08x\n", (a>>1));
+  printf("a>>1: "); binprint(a<<1); printf("\n");
 
   return 0;
 }
