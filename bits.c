be7314a2fbc66364bac7c89eae86177123140dc9
Danielis Joniskis <dananasas@gmit.ie>
2019-03-23 18:26:32 +0000

    displaying a and b in hexidecimal

--- bits.c
+++ bits.c
@@ -14,26 +14,30 @@ void binprint(uint32_t x){
 
 int main(int argc, char *argv[]){
 
-  uint32_t a = 1;
-  uint32_t b = 2;
+  uint32_t a = 0x428a2f98;
+  uint32_t b = 0x71374491;
  
+  printf("hex a: %08x\n", a);
+  printf("hex b: %08x\n", b);
+
+
 //printf(" a: %08x\n", a);
-  printf("   a: "); binprint(a);    printf("\n");
+  printf("    a: "); binprint(a);    printf("\n");
 //printf(" b: %08x\n", b);
-  printf("   b: "); binprint(b);    printf("\n");
+  printf("    b: "); binprint(b);    printf("\n");
 
 //printf(" &: %08x\n", (a&b));
-  printf(" a&b: "); binprint(a&b);  printf("\n");
+  printf("  a&b: "); binprint(a&b);  printf("\n");
 //printf(" |: %08x\n", (a|b));
-  printf(" a|b: "); binprint(a|b);  printf("\n");
+  printf("  a|b: "); binprint(a|b);  printf("\n");
 //printf(" ^: %08x\n", (a^b));
-  printf(" a^b: "); binprint(a^b);  printf("\n");
+  printf("  a^b: "); binprint(a^b);  printf("\n");
 //printf(" ~: %08x\n", (~a));
-  printf("  ~a: "); binprint(~a);   printf("\n");
+  printf("   ~a: "); binprint(~a);   printf("\n");
 //printf("<<: %08x\n", (a<<1));
-  printf("a<<1: "); binprint(a<<1); printf("\n");
+  printf(" a<<1: "); binprint(a<<1); printf("\n");
 //printf(">>: %08x\n", (a>>1));
-  printf("a>>1: "); binprint(a<<1); printf("\n");
+  printf(" a>>1: "); binprint(a>>1); printf("\n");
 
   return 0;
 }
