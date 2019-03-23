8d8312dcd18fbc732c2a9e45c96d273b17e963ad
Danielis Joniskis <dananasas@gmit.ie>
2019-03-23 16:15:10 +0000

    showing bit level operators

--- bits.c
+++ bits.c
@@ -5,6 +5,16 @@ int main(int argc, char *argv[]){
 
   uint32_t a = 1;
   uint32_t b = 2;
+ 
+  printf(" a: %08x\n", a);
+  printf(" b: %08x\n", b);
+
+  printf(" &: %08x\n", (a&b));
+  printf(" |: %08x\n", (a|b));
+  printf(" ^: %08x\n", (a^b));
+  printf(" ~: %08x\n", (~a));
+  printf("<<: %08x\n", (a<<1));
+  printf(">>: %08x\n", (a>>1));
 
   return 0;
 }
