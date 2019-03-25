2b92bb2099fb7add7ac7b0ec904e3755df1a8dae
Danielis Joniskis <dananasas@gmit.ie>
2019-03-25 16:39:43 +0000

    opening file, reading byte and closing using the command line arguement

--- sha256/padfile.c
+++ sha256/padfile.c
@@ -2,7 +2,16 @@
 #include <stdint.h>
 
 int main(int argc, char *argv[]){
+  
+  FILE* f;
 
+  char c;
+  
+  f = fopen(argv[1], "r");
+
+  printf("%c\n", fread(&c, 1, 1, f));
+
+  fclose(f);
 
   return 0;
 }
