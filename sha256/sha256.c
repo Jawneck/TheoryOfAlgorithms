8326c8e52210d1e5752e486074d9d0ebe752b0a4
Danielis Joniskis <dananasas@gmit.ie>
2019-03-27 17:27:37 +0000

    added padfile.c into sha256.c

--- sha256/sha256.c
+++ sha256/sha256.c
@@ -20,6 +20,9 @@ enum status {READ, PAD0, PAD1, FINISH};
 
 void sha256();
 
+  FILE* f;
+  f = fopen(argv[1], "r");
+
 //See Sections 4.1.2 for definitions.
 uint32_t sig0(uint32_t x);
 uint32_t sig1(uint32_t x);
@@ -36,7 +39,8 @@ uint32_t SIG1(uint32_t x);
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
 uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
 
-
+//Retrieves the next message block.
+int nextmsgblock(FILE *f, union msgblock *M, enum status *S, int *nobits);
 
 int main(int argc, char *argv[]){
   
@@ -175,14 +179,59 @@ uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
   return ((x & y) ^ (x & z) ^ (y & z));
 }
 
+int nextmsgblock(FILE *f, union msgblock *M, enum status *S, int *nobits) {
+  
+  union msgblock M;
 
+  uint64_t nobits = 0;
 
+  uint64_t nobytes;
 
+  enum status S = READ;
 
+  int i;  
 
+  while (S == READ) {
+    nobytes = fread(M.e, 1, 64, f);
+    printf("Read %2llu bytes\n", nobytes);
+    nobits = nobits + (nobytes * 8);
+    if(nobytes < 56) {
+      printf("I've found a block with less than 55 bytes!\n");
+      M.e[nobytes] = 0x80;
+      while(nobytes < 56){
+        nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00;
+      }
+      M.s[7] = nobits;
+      S = FINISH;
+    } else if (nobytes < 64){
+      S = PAD0;
+      M.e[nobytes] = 0x80;
+      while (nobytes < 64){
+        nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00;
+      }
+    } else if (feof(f)){
+      S = PAD1;
+    }     
+  }
 
+  if (S == PAD0 || S == PAD1){
+    for (i = 0; i < 56; i++)
+      M.e[i] = 0x00;
+    M.s[7] = nobits;
+  }
+  if (S == PAD1)
+    M.e[0] = 0x80;
 
+  fclose(f);
+  
+  for(int i = 0; i < 64; i++)
+    printf("%x ", M.e[i]);
+  printf("\n");
 
+  return 0;
+}
 
 
 
